9be9ef3ed1e9ae6a33007a10209ccca0f752fac4
Tetyana Khomenko <tetyanakhomenko2002@gmail.com>
2021-07-21 14:34:30 +0300

    Add homework#2

---
+++ hw2code.py
@@ -0,0 +1,9 @@
+a = input("Enter integer number:")
+print(a, type(a))
+print(int(a), type(int(a)))
+
+b = input("Enter float number:")
+print(b, type(b))
+print(float(b), type(float(b)))
+print(int(float(b)), type(int(float(b))))
+
