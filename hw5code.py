e3213b61f15001f6c08f87722c6ff28f4e2d37db
TetyanaKhomenko <87753182+TetyanaKhomenko@users.noreply.github.com>
2021-08-03 01:42:40 +0300

    Add files via upload

---
+++ hw5code.py
@@ -0,0 +1,46 @@
+
+# 5) У вас есть переменная my_str, тип - str. Напечатать все символы, стоящие на четных местах (начиная с нуля).
+
+#####################################################1
+# value = int(input("Enter a number:"))
+# if value < 100:
+#     new_value = value / 2
+# else:
+#     new_value = -value
+# print(new_value)
+
+#####################################################2
+# value = int(input("Enter a number:"))
+# if value < 100:
+#     new_value = 1
+# else:
+#     new_value = 0
+# print(new_value)
+
+#####################################################3
+# value = int(input("Enter a number:"))
+# if value < 100:
+#     new_value = True
+# else:
+#     new_value = False
+# print(new_value)
+
+#####################################################4
+# my_str = input("Enter a string:")
+# print(my_str[::2])
+
+#####################################################5
+
+#####################################################6
+# my_str = input("Enter a string:")
+# if len(my_str) >= 5:
+#     print(my_str)
+# else:
+#     print(my_str * 2)
+
+#####################################################7
+# my_str = input("Enter a string:")
+# if len(my_str) >= 5:
+#     print(my_str)
+# else:
+#     print(my_str + my_str[::-1])
